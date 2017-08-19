0fb2b2a9de8b8c139eb4bd8e67dd00ba3303762d
Victor Zinchenko <zinchenko.us@gmail.com>
2017-08-19 18:38:12 +0300

    Subscribe logic

--- frontend/modules/user/controllers/ProfileController.php
+++ frontend/modules/user/controllers/ProfileController.php
@@ -12,8 +12,12 @@ class ProfileController extends Controller
 
     public function actionView($nickname)
     {
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+        
         return $this->render('view', [
             'user' => $this->findUser($nickname),
+            'currentUser' => $currentUser,
         ]);
     }
 
@@ -30,6 +34,50 @@ class ProfileController extends Controller
         throw new NotFoundHttpException();
     }
     
+    public function actionSubscribe($id)
+    {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/user/default/login']);
+        }
+        
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+        $user = $this->findUserById($id);
+        
+        $currentUser->followUser($user);
+        
+        return $this->redirect(['/user/profile/view', 'nickname' => $user->getNickname()]);        
+    }
+    
+    
+    public function actionUnsubscribe($id)
+    {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/user/default/login']);
+        }
+
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+        $user = $this->findUser($id);
+
+        $currentUser->unfollowUser($user);
+
+        return $this->redirect(['/user/profile/view', 'nickname' => $user->getNickname()]);
+    }
+
+    /**
+     * @param integer $nickname
+     * @return User
+     * @throws NotFoundHttpException
+     */
+    private function findUserById($id)
+    {
+        if ($user = User::findOne($id)) {
+            return $user;
+        }
+        throw new NotFoundHttpException();
+    }
+
 //    public function actionGenerate()
 //    {
 //        $faker = \Faker\Factory::create();
