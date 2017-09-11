c8cde68b92a73e22fdf44e32c681d90fb751e3a8
Victor Zinchenko <zinchenko.us@gmail.com>
2017-09-11 12:51:32 +0300

    Post entity Create post action

---
+++ frontend/models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $filename
+ * @property string $description
+ * @property integer $created_at
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'filename' => 'Filename',
+            'description' => 'Description',
+            'created_at' => 'Created At',
+        ];
+    }
+}
