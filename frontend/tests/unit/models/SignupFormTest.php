329f55d299b0bc676ffcd11870484da4294b271c
Victor Zinchenko <zinchenko.us@gmail.com>
2017-10-05 15:37:30 +0300

    Unit tests

--- frontend/tests/unit/models/SignupFormTest.php
+++ frontend/tests/unit/models/SignupFormTest.php
@@ -1,8 +1,8 @@
 <?php
-namespace frontend\tests\unit\models;
+namespace frontend\tests\models;
 
-use common\fixtures\UserFixture;
-use frontend\models\SignupForm;
+use frontend\tests\fixtures\UserFixture;
+use frontend\modules\user\models\SignupForm;
 
 class SignupFormTest extends \Codeception\Test\Unit
 {
@@ -10,50 +10,85 @@ class SignupFormTest extends \Codeception\Test\Unit
      * @var \frontend\tests\UnitTester
      */
     protected $tester;
-
-
+    
     public function _before()
     {
         $this->tester->haveFixtures([
             'user' => [
                 'class' => UserFixture::className(),
-                'dataFile' => codecept_data_dir() . 'user.php'
             ]
         ]);
     }
-
-    public function testCorrectSignup()
+    
+    public function testTrimUsername()
     {
         $model = new SignupForm([
-            'username' => 'some_username',
+            'username' => ' some_username ',
+            'email' => 'some_email@example.com',
+            'password' => '12345',
+        ]);
+
+        $model->signup();
+
+        expect($model->username)
+                ->equals('some_username');
+    }
+    
+    public function testUsernameRequired()
+    {
+        $model = new SignupForm([
+            'username' => '',
             'email' => 'some_email@example.com',
             'password' => 'some_password',
         ]);
 
-        $user = $model->signup();
+        $model->signup();
 
-        expect($user)->isInstanceOf('common\models\User');
-
-        expect($user->username)->equals('some_username');
-        expect($user->email)->equals('some_email@example.com');
-        expect($user->validatePassword('some_password'))->true();
+        expect($model->getFirstError('username'))
+                ->equals('Username cannot be blank.');
     }
-
-    public function testNotCorrectSignup()
+    
+    public function testUsernameTooShort()
     {
         $model = new SignupForm([
-            'username' => 'troy.becker',
-            'email' => 'nicolas.dianna@hotmail.com',
+            'username' => 's',
+            'email' => 'some_email@example.com',
             'password' => 'some_password',
         ]);
 
-        expect_not($model->signup());
-        expect_that($model->getErrors('username'));
-        expect_that($model->getErrors('email'));
+        $model->signup();
 
         expect($model->getFirstError('username'))
-            ->equals('This username has already been taken.');
+                ->equals('Username should contain at least 2 characters.');
+    }
+    
+    public function testPasswordRequired()
+    {
+        $model = new SignupForm([
+            'username' => 'some_username',
+            'email' => 'some_email@example.com',
+            'password' => '',
+        ]);
+
+        $model->signup();
+
+        expect($model->getFirstError('password'))
+                ->equals('Password cannot be blank.');
+    }    
+
+    public function testEmailUnique()
+    {
+        $model = new SignupForm([
+            'username' => 'some_username',
+            'email' => '1@got.com',
+            'password' => 'some_password',
+        ]);
+
+        $model->signup();
+        
+        sleep(10);
+
         expect($model->getFirstError('email'))
-            ->equals('This email address has already been taken.');
+                ->equals('This email address has already been taken.');
     }
 }
